7d35a5f5c1776ed413289ebac76b4b7db0cbc91b
jdgwartney <davidg@boundary.com>
2015-02-08 11:52:42 -0800

    Missing Camel 14.1 eclipse configuration

--- src/test/java/com/boundary/camel/component/ssh/SshComponentSecurityTest.java
+++ src/test/java/com/boundary/camel/component/ssh/SshComponentSecurityTest.java
@@ -18,7 +18,6 @@ package com.boundary.camel.component.ssh;
 
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
-import org.apache.camel.component.ssh.FileKeyPairProvider;
 import org.apache.sshd.common.KeyPairProvider;
 import org.apache.sshd.common.keyprovider.FileKeyPairProvider;
 import org.junit.Test;
